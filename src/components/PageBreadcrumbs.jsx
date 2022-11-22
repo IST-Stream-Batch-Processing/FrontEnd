932306c6e834c42c208bdd7ea09668080098c31c
Naomi <41462684+naomixie@users.noreply.github.com>
2022-11-23 05:01:45 +0800

    feat: 新增界面breadCrumbs
    Signed-off-by: Naomi <41462684+naomixie@users.noreply.github.com>

---
+++ src/components/PageBreadcrumbs.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Breadcrumb} from "antd";
+import {useLocation} from "react-router-dom";
+import {css} from "aphrodite";
+import styles from '../constants/pageStyle';
+
+// custom hook to get the current pathname in React
+export const getPathname = () => {
+    const location = useLocation();
+    console.log(location.pathname);
+    return location.pathname;
+};
+
+const breadcrumbsMapping = {
+    '/developer/layouts': '界面',
+    '/developer/streamProcess/model': '流处理服务/流数据源',
+    '/developer/streamProcess/service': '流处理服务/流数据处理'
+};
+
+const PageBreadcrumbs = ({routes}) => (
+        <Breadcrumb className={css(styles.breadCrumbs)}>
+            {breadcrumbsMapping[getPathname()] ? breadcrumbsMapping[getPathname()].split("/").map((item) => <Breadcrumb.Item>{item}</Breadcrumb.Item>) : null}
+        </Breadcrumb>
+    );
+
+export default PageBreadcrumbs;
