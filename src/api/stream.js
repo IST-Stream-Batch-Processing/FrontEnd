a9e975fae28143781fb1894b36cb314c80070815
Naomi <41462684+naomixie@users.noreply.github.com>
2022-12-06 03:11:52 +0800

    feat: 流数据服务与后端Operator类的集成
    Signed-off-by: Naomi <41462684+naomixie@users.noreply.github.com>

--- src/api/stream.js
+++ src/api/stream.js
@@ -11,10 +11,20 @@ const deleteModelData = (id) => api.delete(`${streamURL}/data/${id}`).then(dataH
 const getSingleModelData = (id) => api.get(`${streamURL}/data/${id}`).then(dataHandler);
 
 const updateModelData = (data) => api.put(`${streamURL}/data`, data).then(dataHandler);
+
+const getAllServiceData = () => api.get(`${streamURL}/operator`).then(dataHandler);
+
+const deleteServiceData = (id) => api.delete(`${streamURL}/operator/${id}`).then(dataHandler);
+
+const getSingleServiceData = (id) => api.get(`${streamURL}/operator/${id}`).then(dataHandler);
+
 export {
     getAllModelData,
     createModelData,
     deleteModelData,
     getSingleModelData,
-    updateModelData
+    updateModelData,
+    getAllServiceData,
+    deleteServiceData,
+    getSingleServiceData
 };
